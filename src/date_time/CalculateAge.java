efbc65ec9bd2a70c34235bc0c83d2570f6e4fc30
kidu21 <kidu2017@gmail.com>
2020-10-20 11:41:38 +0200

    Commit exercise01

---
+++ src/date_time/CalculateAge.java
@@ -0,0 +1,20 @@
+
+/*
+ * Write a Java program to calculate your age
+ */
+package date_time;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class CalculateAge {
+	
+	protected void calculateAge() {
+		LocalDate myBirthday = LocalDate.of(1988, 6, 18);
+		LocalDate currentTime = LocalDate.now();
+		
+		Period period = Period.between(myBirthday, currentTime);
+		
+		System.out.printf("I am %d years %d months %d days old \n ", period.getYears(),period.getMonths(), period.getDays());
+	}
+}
---
+++ src/date_time/Main.java
@@ -0,0 +1,22 @@
+
+
+package date_time;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		// Months remaining in this year
+		RemainingMonths months = new RemainingMonths();
+		
+		months.remainingMonths();
+		
+		// My age
+		CalculateAge age = new CalculateAge();
+		age.calculateAge();
+
+	}
+
+
+}
